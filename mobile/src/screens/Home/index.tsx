2a393d0cf1d97eb11509b106c0af8a19e71731e5
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-09-14 06:33:58 -0300

    feat: :sparkles: primeiro card

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { View, Image } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
 
+import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
@@ -12,6 +15,16 @@ export function Home() {
         source={logoImg}
         style={styles.logo}
       />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <GameCard
+        data={GAMES[0]}
+      />
+
     </View>
   );
 }
